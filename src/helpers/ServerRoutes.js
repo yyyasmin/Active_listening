ade6973210e776d3fa1a0baa4bcc200f05c27d03
unknown <you@example.com>
2024-01-08 08:41:14 +0200

    server side path

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -1,8 +1,8 @@
 export const PROXY_URL = "http://localhost:5000";
 export const RENDER_PROXY_URL = "https://mg-production.up.railway.app";
 
-//export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
-export const CHOSEN_PROXY_URL = PROXY_URL;
+export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
+//export const CHOSEN_PROXY_URL = PROXY_URL;
 
 export const TITLE_SIZE = "80rem";  // JUST FOR CARDS AREA HEIGHT CALCULATION
 
